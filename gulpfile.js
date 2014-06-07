7a6d76232fd2c206b01db62a0db3b6f2a726d1dd
Лаврёнов Антон <lavrton@gmail.com>
2014-06-07 14:04:13 +0800

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var gutil = require('gulp-util');
+var browserify = require('gulp-browserify');
+var livereload = require('gulp-livereload');
+
+// Basic usage
+gulp.task('scripts', function() {
+    // Single entry point to browserify
+    gulp.src('scripts/app.js')
+        .pipe(browserify({
+          debug : true,
+        }).on('error', gutil.log))
+        .pipe(gulp.dest('./build'))
+        .pipe(livereload());
+});
+
+gulp.task('watch', function() {
+  gulp.watch(['scripts/**/*.js', 'scripts/**/*.html'], ['scripts']);
+});
+
+gulp.task('connect', function() {
+  connect.server({
+    port: 8000
+  });
+});
+
+gulp.task('default', ['scripts', 'watch', 'connect']);
